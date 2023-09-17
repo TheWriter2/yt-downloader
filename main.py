aebd42dbe1b0ae4cc21bcfdf65deb365f8b17bb1
TheWriter <netto9903@gmail.com>
2023-09-17 11:50:23 -0300

    reorganized main script

--- main.py
+++ main.py
@@ -3,20 +3,27 @@ import sys
 import choice
 import yt_dlp
 
+# Getting current directory
 os.chdir(os.path.dirname(sys.argv[0]))
 
+# Getting the folder to download the files into
 try:
     os.chdir("./downloads/")
 except:
     os.mkdir("downloads")
     os.chdir("./downloads/")
 
+# Main class
 class VideoDownloader:
     def __init__(self):
+        
+        # Video and audio
         self.Downloader = yt_dlp.YoutubeDL({
             "format":"mp4/bv*[height<=480]+ba/b[height<=480] / wv*+ba/w",
             "ignoreerrors": True
         })
+
+        # Audio only
         self.Downloader_MP3 = yt_dlp.YoutubeDL({
             "format":"mp3/bestaudio/best",
             "postprocessors": [{  # Extract audio using ffmpeg
@@ -25,15 +32,20 @@ class VideoDownloader:
             }],
             "ignoreerrors": True
         })
+
+        # Flags
+        self.downloadMP3 = False
     def mainmenu(self):
         choosing = choice.inputchoice(["Download Video", "Download Video (audio only)", "Exit"], "Welcome! What do you want to do?")
 
         if choosing == 3:
             sys.exit()
         elif choosing == 1:
+            self.downloadMP3 = False
             self.download()
         elif choosing == 2:
-            self.download_mp3()
+            self.downloadMP3 = True
+            self.download()
     
     def download(self):
         link = input("Copy & paste the URL of the youtube video you want to download:\n")
@@ -45,31 +57,17 @@ class VideoDownloader:
             os.mkdir(playlist_info.get("title"))
             os.chdir("./{}/".format(playlist_info.get("title")))
 
-            self.Downloader.download([link])
-            os.chdir("../")
-        else:
+        if self.downloadMP3 == False:
             self.Downloader.cache.remove()
             self.Downloader.download([link])
-    
-    def download_mp3(self):
-        link = input("Copy & paste the URL of the youtube video you want to download:\n")
-
-        if link[24:32] == "playlist":
-            self.Downloader_MP3.cache.remove()
-            playlist_info = self.Downloader_MP3.extract_info(link, download=False)
-
-            print(playlist_info.get("title"))
-
-            os.mkdir(playlist_info.get("title") + "_MP3")
-            os.chdir("./{}/".format(playlist_info.get("title") + "_MP3"))
-
-            print(os.getcwd())
-
-            self.Downloader_MP3.download([link])
-            os.chdir("../")
         else:
             self.Downloader_MP3.cache.remove()
             self.Downloader_MP3.download([link])
+        
+        if link[24:32] == "playlist":
+            os.chdir("..")
+        
+        self.mainmenu()
 
 if __name__ == "__main__":
     cur_run = VideoDownloader()
