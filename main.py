9cae356d659a683aa217e9e729ce83411b6bd1c5
TheWriter2 <64555811+TheWriter2@users.noreply.github.com>
2023-11-09 12:24:14 -0300

    Repository reboot

--- main.py
+++ main.py
@@ -1,94 +1,34 @@
-import os
-import sys
-import choice
-import yt_dlp
+import PySimpleGUI as sg
 
-# Getting current directory
-os.chdir(os.path.dirname(sys.argv[0]))
+layouts = {
+    "main":[]
+}
 
-# Getting the folder to download the files into
-try:
-    os.chdir("./downloads/")
-except:
-    os.mkdir("downloads")
-    os.chdir("./downloads/")
+layouts["main"] = [
+    # Row 1
+    [
+        sg.Text("Hello world!")
+    ],
+    # Row 2
+    [
+        sg.Button("OK", key="okButton")
+    ]
+]
 
-# Main class
-class VideoDownloader:
-    def __init__(self):
+mainWindow = sg.Window("Test", layouts["main"])
 
-        # Video/Audio data        
-        self.videoDict = {
-            "format":"mp4",
-            #"format-sort":"+size,+br,+res,+fps",
-            "ignoreerrors": True
-        }
+# Main loop
+while True:
+    winEvent, winEventValues = mainWindow.read()
 
-        self.audioDict = {
-            "format":"mp3",
-            "postprocessors": [{  # Extract audio using ffmpeg
-                "key": "FFmpegExtractAudio",
-                "preferredcodec": "mp3",
-            }],
-            "ignoreerrors": True
-        }
+    # Event handling
+    if winEvent == sg.WIN_CLOSED:
+        print("App terminated, close button.")
+        break
 
-        # Video and audio
-        self.Downloader = yt_dlp.YoutubeDL(self.videoDict)
+    if winEvent == "okButton":
+        print("App terminated, button click.")
+        break
 
-        # Audio only
-        self.Downloader_MP3 = yt_dlp.YoutubeDL(self.audioDict)
-
-        # Flags
-        self.downloadMP3 = False
-        self.videoFormats = ["flv", "m4a", "mp4", "webm"]
-        self.audioFormats = ["3gp", "aac", "mp3", "ogg", "wav"]
-    def mainmenu(self):
-        choosing = choice.inputchoice(["Download Video", "Download Video (audio only)", "Exit"], "Welcome! What do you want to do?")
-
-        if choosing == 3:
-            sys.exit()
-        elif choosing == 1:
-            self.downloadMP3 = False
-            self.download()
-        elif choosing == 2:
-            self.downloadMP3 = True
-            self.download()
-    
-    def download(self):
-        link = input("Copy & paste the URL of the youtube video you want to download:\n")
-        
-        # Playlist folder
-        if link[24:32] == "playlist":
-            self.Downloader.cache.remove()
-            playlist_info = self.Downloader.extract_info(link, download=False)
-            
-            os.mkdir(playlist_info.get("title"))
-            os.chdir("./{}/".format(playlist_info.get("title")))
-
-        # Downloading video
-        if self.downloadMP3 == False:
-            choosing = choice.inputchoice(self.videoFormats, "Select a format:")
-
-            self.videoDict["format"] = self.videoFormats[choosing - 1] + "/b*"
-            self.Downloader = yt_dlp.YoutubeDL(self.videoDict)
-
-            self.Downloader.cache.remove()
-            self.Downloader.download([link])
-        else:
-            choosing = choice.inputchoice(self.audioFormats, "Select a format:")
-
-            self.audioDict["format"] = self.audioFormats[choosing - 1] + "/b*"
-            self.Downloader_MP3 = yt_dlp.YoutubeDL(self.audioDict)
-
-            self.Downloader_MP3.cache.remove()
-            self.Downloader_MP3.download([link])
-        
-        if link[24:32] == "playlist":
-            os.chdir("..")
-        
-        self.mainmenu()
-
-if __name__ == "__main__":
-    cur_run = VideoDownloader()
-    cur_run.mainmenu()
+# End app
+mainWindow.close()
